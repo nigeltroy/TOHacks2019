3313bc68f5cefde5b9bb7a45a194b2145809a6ec
Nancy Ngo <nancyngo141@gmail.com>
2019-06-23 00:57:49 -0400

    updated js

--- scripts.js
+++ scripts.js
@@ -1,25 +1,44 @@
-// Create request
-var directionsService = new google.maps.DirectionsService;
-
-var origin = "Disneyland";
-var destination = "Universal Studios Hollywood";
-
-//request.open('GET', 'https://maps.googleapis.com/maps/api/directions/json?key=AIzaSyDtgSb2PZdgvvplLteWJmVRBKe2eXH-AgM', true)
-
-//request.onload = main();
-
 function main() {
-    var directionsService = new google.maps.DirectionsService;
-
+    // set up
     var url;
     chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs) {
         url = tabs[0].url;
     });
     var arr = parseUri(url);
 
-    //get locations
-    var carDistance = getDistance(arr[0], arr[1], "DRIVING");
-    var busDistance = getDistance(arr[0], arr[1], "TRANSIT");
+    var key = "AIzaSyDtgSb2PZdgvvplLteWJmVRBKe2eXH-AgM";
+    var origin = arr[0];
+    var destination = arr[1];
+    var travelMode = "driving";
+
+    // driving
+    var requestUri = "https://maps.googleapis.com/maps/api/directions/json?origin=" + origin + "&destination=" + destination + "&mode=" + travelMode + "&key=" + key;
+    request.open('GET', requestUri, true);
+
+    var carDistanceMeters;
+    var busDistanceMeters;
+
+    request.onload = function () {
+        // take the distance
+        var data = this.response;
+        carDistanceMeters = data.routes[0].legs[0].distance.value;
+    }
+
+    request.send();
+
+    // transit
+    travelMode = "transit";
+
+    var requestUri = "https://maps.googleapis.com/maps/api/directions/json?origin=" + origin + "&destination=" + destination + "&mode=" + travelMode + "&key=" + key;
+    request.open('GET', requestUri, true);
+
+    request.onload = function () {
+        // take the distance
+        var data = this.response;
+        busDistanceMeters = data.routes[0].legs[0].distance.value;
+    }
+
+    request.send();
 }
 
 function parseUri(uri) {
@@ -27,20 +46,6 @@ function parseUri(uri) {
     var arr = [uriArr[6], uriArr[7]];
 
     return arr;
-}
-
-function getDistance(origin, destination, travelMode) {
-    directionsService.route({
-        origin: origin,
-        destination: destination,
-        travelMode: travelMode
-    });
-
-
-
-    var distance;
-
-    return distance;
 }
 
 
@@ -75,38 +80,3 @@ function calculateAndDisplayRoute(directionsDisplay, directionsService,
         }
     });
 }
-
-
-
-
-
-var request = new XMLHttpRequest()
-
-var baseUri = 'https://maps.googleapis.com/maps/api/directions/json'
-var params = {
-
-}
-// Set method and URI
-request.open('GET', 'https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key=AIzaSyDtgSb2PZdgvvplLteWJmVRBKe2eXH-AgM', true)
-
-// Access JSON response data
-request.onload = function () {
-    // Parse JSON into an object
-    var data = JSON.parse(this.response)
-
-    if (request.status >= 200 && request.status < 400)
-    {
-        // Do stuff with the data
-    }
-    else
-    {
-        console.log('error')
-    }
-
-    chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs) {
-        var url = tabs[0].url;
-    });
-}
-
-// Send request
-request.send()
