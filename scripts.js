df7aeabcb475c33e33308a01e225ef0819b4f521
Nancy Ngo <nancyngo141@gmail.com>
2019-06-23 04:30:16 -0400

    testing

--- scripts.js
+++ scripts.js
@@ -7,50 +7,56 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 function main() {
     // set up
-    var url;
+    var url = "";
     chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs) {
         url = tabs[0].url;
     });
     var arr = parseUri(url);
 
     var key = "AIzaSyDtgSb2PZdgvvplLteWJmVRBKe2eXH-AgM";
-    var origin = arr[0];
-    var destination = arr[1];
+    var origin = "ICON+330,+330+Phillip+St,+Waterloo,+ON+N2L+3W9,+Canada";
+    var destination = "Shoppers+Drug+Mart,+260+Queen+St+W,+Toronto,+ON+M5V+1Z8";
     var travelMode = "driving";
 
     // driving
-    var requestUri = "https://maps.googleapis.com/maps/api/directions/json?origin=" + origin + "&destination=" + destination + "&mode=" + travelMode + "&key=" + key;
-    request.open('GET', requestUri, true);
+    var carRequestUri = "https://maps.googleapis.com/maps/api/directions/json?origin=" + origin + "&destination=" + destination + "&mode=" + travelMode + "&key=" + key;
+    var carRequest = new XMLHttpRequest();
+    carRequest.open('GET', carRequestUri, true);
+    //carRequest.setRequestHeader('Access-Control-Allow-Headers', '*');
 
-    var carDistanceMeters;
-    var busDistanceMeters;
+    var carDistanceMeters = 10;
+    var busDistanceMeters = 11;
 
-    request.onload = function () {
-        // take the distance
-        var data = this.response;
-        carDistanceMeters = data.routes[0].legs[0].distance.value;
-    }
+    //carRequest.onload = parseJson(this.response);
+    var data = JSON.parse(this.response);
+    carDistanceMeters = data.routes[0].legs[0].distance.value;
 
-    request.send();
-
+    console.log(carDistanceMeters);
+    carRequest.send();
+    
     // transit
     travelMode = "transit";
 
-    var requestUri = "https://maps.googleapis.com/maps/api/directions/json?origin=" + origin + "&destination=" + destination + "&mode=" + travelMode + "&key=" + key;
-    request.open('GET', requestUri, true);
+    var busRequestUri = "https://maps.googleapis.com/maps/api/directions/json?origin=" + origin + "&destination=" + destination + "&mode=" + travelMode + "&key=" + key;
+    var busRequest = new XMLHttpRequest();
+    busRequest.open('GET', busRequestUri, true);
+    //busRequest.setRequestHeader('Access-Control-Allow-Headers', '*');
 
-    request.onload = function () {
-        // take the distance
-        var data = this.response;
-        busDistanceMeters = data.routes[0].legs[0].distance.value;
+    busRequest.onload = function () {
+        parseJson(this.response);
     }
+    var data = JSON.parse(this.response);
+    busDistanceMeters = data.routes[0].legs[0].distance.value;
 
-    request.send();
+    console.log(busDistanceMeters);
+    busRequest.send();
 
     var emissionArray = emissionCal(carDistanceMeters, busDistanceMeters);
 
-    document.getElementById('carEmissions').innerHTML = emissionArray[0];
-    document.getElementById('busEmissions').innerHTML = emissionArray[1];
+    //document.getElementById('carEmissions').innerHTML = emissionArray[0];
+    //document.getElementById('busEmissions').innerHTML = emissionArray[1];
+
+    return busDistanceMeters;
 }
 
 function parseUri(uri) {
@@ -66,7 +72,13 @@ function emissionCal(distance1, distance2) {
 
 function showRes() {
     var printed = document.getElementById("carEmissions");
-    printed.innerText = main()[0];
-    //var printing = document.getElementById("busEmissions");
-    //printing.innerText = main()[1]
+    printed.innerText = main();
+    var printing = document.getElementById("busEmissions");
+    printing.innerText = main();
+}
+
+function parseJson(response) {
+    // take the distance
+    var data = JSON.parse(response);
+    return data.routes[0].legs[0].distance.value;
 }
